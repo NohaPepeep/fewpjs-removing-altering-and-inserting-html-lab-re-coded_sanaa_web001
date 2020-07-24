f07fed9744fba39fd33621988bf900bc374fbd53
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-07-24 15:38:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,4 @@ element.appendChild(ul)
 element.innerHTML = 'Hello, DOM!';
 let header = document.getElementById("div#header");
 header.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+element.style.backgroundColor = '#27647B';
